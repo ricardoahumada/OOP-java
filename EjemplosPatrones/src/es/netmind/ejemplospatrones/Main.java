8c16d4d54a3b8c28c458222d3297ab9227d987f9
ricardonetmind <ricardoag@expert.netmind.es>
2023-08-19 11:31:03 +0200

    ejemplos patrones

---
+++ EjemplosPatrones/src/es/netmind/ejemplospatrones/Main.java
@@ -0,0 +1,7 @@
+package es.netmind.ejemplospatrones;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ EjemplosPatrones/src/es/netmind/ejemplospatrones/singleton/SingletonClient.java
@@ -0,0 +1,14 @@
+package es.netmind.ejemplospatrones.singleton;
+
+public class SingletonClient {
+    public static void main(String[] args) {
+        System.out.println("Usando un singleton");
+        System.out.println("-----------------------------------");
+
+        MySingletonRepo aRepo = MySingletonRepo.getInstance();
+
+        aRepo.ASingletonBizzMethod();
+        aRepo.AnotherSingletonBizzMethod();
+
+    }
+}
