3d1baca0a5eab314daaa0af9b480ba5628782ef7
ricardonetmind <ricardoag@expert.netmind.es>
2023-10-10 23:42:55 +0200

    update

---
+++ cats/src/main/java/com/cats/Cat.java
@@ -0,0 +1,16 @@
+package com.cats;
+
+public class Cat {
+    private String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Cat() {
+    }
+}
