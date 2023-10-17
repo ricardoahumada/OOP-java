ab035e1090d237a304cf01457388915c13186e10
ricardonetmind <ricardoag@expert.netmind.es>
2023-10-17 22:33:45 +0200

    update

---
+++ ShoppingCart/Solucion/ShoppingCart/src/main/java/com/myonlineshop/App.java
@@ -0,0 +1,13 @@
+package com.myonlineshop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Shopping Cart" );
+    }
+}
---
+++ ShoppingCart/Solucion/ShoppingCart/src/main/java/com/myonlineshop/exception/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package com.myonlineshop.exception;
+
+public class ProductNotFoundException extends Exception {
+	public ProductNotFoundException() {
+		super();
+	}
+}
