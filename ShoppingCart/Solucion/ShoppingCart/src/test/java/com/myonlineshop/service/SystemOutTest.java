98916b88918ab0e2ed4c4c3412d93e9f69f6a36b
ricardonetmind <ricardoag@expert.netmind.es>
2023-10-18 13:59:54 +0200

    update

---
+++ ShoppingCart/Solucion/ShoppingCart/src/test/java/com/myonlineshop/service/SystemOutTest.java
@@ -0,0 +1,40 @@
+package com.myonlineshop.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class SystemOutTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @Test
+    public void out() {
+        System.out.print("hello");
+
+        assertEquals("hello", outContent.toString());
+        assertThat(outContent.toString(), containsString("hel"));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+}
