d9d96fdb1f7a414ece1a985f57517d0cd2790b0b
ricardo <ricardo@enmotionvalue.com>
2023-11-20 13:03:10 +0100

    update

--- SchoolManager/src/main/java/com/banana/config/SpringConfig.java
+++ SchoolManager/src/main/java/com/banana/config/SpringConfig.java
@@ -1,9 +1,13 @@
 package com.banana.config;
 
+import com.banana.persistence.StudentsRepository;
+import com.banana.services.StudentsServiceI;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
-@ComponentScan(basePackages = {"com.banana.persistence", "com.banana.services"})
+//@ComponentScan(basePackages = {"com.banana.persistence", "com.banana.services"})
+@Import({StudentsRepository.class, StudentsServiceI.class})
 public class SpringConfig {
 }
