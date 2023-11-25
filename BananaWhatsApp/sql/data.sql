c4e93551ed7ca8d38e3c10f5cce31b8201a87519
ricardonetmind <ricardoag@expert.netmind.es>
2023-11-25 13:50:05 +0100

    reto2 sbt added

--- BananaWhatsApp/sql/data.sql
+++ BananaWhatsApp/sql/data.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 07, 2023 at 06:41 PM
+-- Generation Time: Nov 25, 2023 at 12:46 PM
 -- Server version: 5.7.11
 -- PHP Version: 5.6.18
 
@@ -17,35 +17,27 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `personalapi_db`
+-- Database: `bananawhatsappdb`
 --
 
 --
--- Dumping data for table `cliente`
+-- Dumping data for table `usuario`
 --
 
-INSERT INTO `cliente` (`dtype`, `id`, `activo`, `alta`, `direccion`, `email`, `moroso`, `nombre`, `cif`, `unidades_de_negocio`, `dni`) VALUES
-('Personal', 1, b'1', '2023-11-07', 'Calle JJ 1', 'jj@j.com', b'0', 'Juan Juanez', NULL, NULL, '12345678J'),
-('Personal', 2, b'1', '2023-11-07', 'Calle LP 2', 'lp@l.com', b'0', 'Luisa Perez', NULL, NULL, '12345678L'),
-('Empresa', 3, b'1', '2023-11-07', 'Calle SI 3', 'si@s.com', b'0', 'Servicios Informatico SL', 'J12345678', NULL, NULL);
+INSERT INTO `usuario` (`id`, `activo`, `alta`, `email`, `nombre`) VALUES
+(1, b'1', '2023-11-25', 'juana@j.com', 'Juana'),
+(2, b'1', '2023-11-25', 'luis@l.com', 'Luis');
 
 --
--- Dumping data for table `cuenta`
+-- Dumping data for table `mensaje`
 --
 
-INSERT INTO `cuenta` (`dtype`, `id`, `comision`, `fecha_creacion`, `interes`, `saldo`, `cliente_id`) VALUES
-('Ahorro', 1, 0.2, '2023-11-07', 1.1, 100, 1),
-('Corriente', 2, 0.2, '2023-11-07', 0.5, 200, 3),
-('Ahorro', 3, 0.2, '2023-11-07', 1.1, 300, 2),
-('Ahorro', 4, 0.2, '2023-11-07', 1.1, 300, 1);
+INSERT INTO `mensaje` (`id`, `cuerpo`, `fecha`, `from_user`, `to_user`) VALUES
+(1, 'Hola, qué tal?', '2023-11-25', 1, 2),
+(2, 'Muy bien! y tu?', '2023-11-25', 2, 1),
+(3, 'Bien también...', '2023-11-25', 1, 2),
+(4, 'Chachi!', '2023-11-25', 2, 1);
 
---
--- Dumping data for table `prestamo`
---
-
-INSERT INTO `prestamo` (`id`, `anios`, `fecha_concesion`, `interes`, `interes_mora`, `liquidado`, `mensualidad`, `monto`, `moroso`, `saldo`, `cliente_id`) VALUES
-(1, 2, '2023-11-07', 4, 2, b'0', NULL, 1000, b'0', 1000, 1),
-(2, 25, '2023-11-07', 4, 2, b'0', NULL, 100000, b'0', 100000, 3);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- BananaWhatsApp/sql/tables_structure.sql
+++ BananaWhatsApp/sql/tables_structure.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 07, 2023 at 06:40 PM
+-- Generation Time: Nov 25, 2023 at 12:45 PM
 -- Server version: 5.7.11
 -- PHP Version: 5.6.18
 
@@ -17,128 +17,37 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `personalapi_db`
+-- Database: `bananawhatsappdb`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cheque`
+-- Table structure for table `mensaje`
 --
 
-DROP TABLE IF EXISTS `cheque`;
-CREATE TABLE `cheque` (
+DROP TABLE IF EXISTS `mensaje`;
+CREATE TABLE `mensaje` (
   `id` int(11) NOT NULL,
-  `beneficiario` varchar(255) DEFAULT NULL,
+  `cuerpo` varchar(255) DEFAULT NULL,
   `fecha` date DEFAULT NULL,
-  `importe` double DEFAULT NULL,
-  `cuenta_id` int(11) DEFAULT NULL
+  `from_user` int(11) DEFAULT NULL,
+  `to_user` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cliente`
+-- Table structure for table `usuario`
 --
 
-DROP TABLE IF EXISTS `cliente`;
-CREATE TABLE `cliente` (
-  `dtype` varchar(31) NOT NULL,
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE `usuario` (
   `id` int(11) NOT NULL,
   `activo` bit(1) NOT NULL,
   `alta` date DEFAULT NULL,
-  `direccion` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
-  `moroso` bit(1) NOT NULL,
-  `nombre` varchar(255) DEFAULT NULL,
-  `cif` varchar(255) DEFAULT NULL,
-  `unidades_de_negocio` varchar(255) DEFAULT NULL,
-  `dni` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cuenta`
---
-
-DROP TABLE IF EXISTS `cuenta`;
-CREATE TABLE `cuenta` (
-  `dtype` varchar(31) NOT NULL,
-  `id` int(11) NOT NULL,
-  `comision` double DEFAULT NULL,
-  `fecha_creacion` date DEFAULT NULL,
-  `interes` double DEFAULT NULL,
-  `saldo` double DEFAULT NULL,
-  `cliente_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cuenta_transacciones`
---
-
-DROP TABLE IF EXISTS `cuenta_transacciones`;
-CREATE TABLE `cuenta_transacciones` (
-  `cuenta_id` int(11) NOT NULL,
-  `transacciones_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `mora`
---
-
-DROP TABLE IF EXISTS `mora`;
-CREATE TABLE `mora` (
-  `id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `pago`
---
-
-DROP TABLE IF EXISTS `pago`;
-CREATE TABLE `pago` (
-  `id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `prestamo`
---
-
-DROP TABLE IF EXISTS `prestamo`;
-CREATE TABLE `prestamo` (
-  `id` int(11) NOT NULL,
-  `anios` int(11) DEFAULT NULL,
-  `fecha_concesion` date DEFAULT NULL,
-  `interes` int(11) DEFAULT NULL,
-  `interes_mora` int(11) DEFAULT NULL,
-  `liquidado` bit(1) NOT NULL,
-  `mensualidad` double DEFAULT NULL,
-  `monto` double DEFAULT NULL,
-  `moroso` bit(1) NOT NULL,
-  `saldo` double DEFAULT NULL,
-  `cliente_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `transaccion`
---
-
-DROP TABLE IF EXISTS `transaccion`;
-CREATE TABLE `transaccion` (
-  `id` int(11) NOT NULL,
-  `fecha` date DEFAULT NULL,
-  `importe` double DEFAULT NULL
+  `nombre` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -146,55 +55,17 @@ CREATE TABLE `transaccion` (
 --
 
 --
--- Indexes for table `cheque`
+-- Indexes for table `mensaje`
 --
-ALTER TABLE `cheque`
+ALTER TABLE `mensaje`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `FKju68ghku0sfdjore2ao4ip4to` (`cuenta_id`);
+  ADD KEY `FKe575t9q4ol4vdkb21f4tysbxe` (`from_user`),
+  ADD KEY `FK76kstlgspgoe0kdekwggfvtd2` (`to_user`);
 
 --
--- Indexes for table `cliente`
+-- Indexes for table `usuario`
 --
-ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `cuenta`
---
-ALTER TABLE `cuenta`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FK4p224uogyy5hmxvn8fwa2jlug` (`cliente_id`);
-
---
--- Indexes for table `cuenta_transacciones`
---
-ALTER TABLE `cuenta_transacciones`
-  ADD UNIQUE KEY `UK_79dxuir9ppn0esbo5erd6hj1o` (`transacciones_id`),
-  ADD KEY `FKfgm4d29rn5bl1kxbnyr44ihcg` (`cuenta_id`);
-
---
--- Indexes for table `mora`
---
-ALTER TABLE `mora`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `pago`
---
-ALTER TABLE `pago`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `prestamo`
---
-ALTER TABLE `prestamo`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FKnuxm078vsm77xihcgtkk4cnns` (`cliente_id`);
-
---
--- Indexes for table `transaccion`
---
-ALTER TABLE `transaccion`
+ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -202,68 +73,25 @@ ALTER TABLE `transaccion`
 --
 
 --
--- AUTO_INCREMENT for table `cheque`
+-- AUTO_INCREMENT for table `mensaje`
 --
-ALTER TABLE `cheque`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `cliente`
---
-ALTER TABLE `cliente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `cuenta`
---
-ALTER TABLE `cuenta`
+ALTER TABLE `mensaje`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
--- AUTO_INCREMENT for table `mora`
+-- AUTO_INCREMENT for table `usuario`
 --
-ALTER TABLE `mora`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `pago`
---
-ALTER TABLE `pago`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `prestamo`
---
-ALTER TABLE `prestamo`
+ALTER TABLE `usuario`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `transaccion`
---
-ALTER TABLE `transaccion`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `cheque`
+-- Constraints for table `mensaje`
 --
-ALTER TABLE `cheque`
-  ADD CONSTRAINT `FKju68ghku0sfdjore2ao4ip4to` FOREIGN KEY (`cuenta_id`) REFERENCES `cuenta` (`id`);
-
---
--- Constraints for table `cuenta`
---
-ALTER TABLE `cuenta`
-  ADD CONSTRAINT `FK4p224uogyy5hmxvn8fwa2jlug` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`);
-
---
--- Constraints for table `cuenta_transacciones`
---
-ALTER TABLE `cuenta_transacciones`
-  ADD CONSTRAINT `FK4xlif3958u92760rb4udgmix5` FOREIGN KEY (`transacciones_id`) REFERENCES `transaccion` (`id`),
-  ADD CONSTRAINT `FKfgm4d29rn5bl1kxbnyr44ihcg` FOREIGN KEY (`cuenta_id`) REFERENCES `cuenta` (`id`);
-
---
--- Constraints for table `prestamo`
---
-ALTER TABLE `prestamo`
-  ADD CONSTRAINT `FKnuxm078vsm77xihcgtkk4cnns` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`);
+ALTER TABLE `mensaje`
+  ADD CONSTRAINT `FK76kstlgspgoe0kdekwggfvtd2` FOREIGN KEY (`to_user`) REFERENCES `usuario` (`id`),
+  ADD CONSTRAINT `FKe575t9q4ol4vdkb21f4tysbxe` FOREIGN KEY (`from_user`) REFERENCES `usuario` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
