3bf46a8e79935e311476092190d60ee3b5555544
ricardo <ricardo@enmotionvalue.com>
2023-11-27 11:07:57 +0100

    update

--- SchoolManager/src/main/java/com/banana/config/SpringConfig.java
+++ SchoolManager/src/main/java/com/banana/config/SpringConfig.java
@@ -1,13 +1,13 @@
 package com.banana.config;
 
-import org.springframework.context.annotation.*;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({ReposConfig.class, ServicesConfig.class})
+@Import({PropertiesConfig.class, PropertiesConfigDev.class, ReposConfig.class, ServicesConfig.class})
 @ComponentScan(basePackages = {"com.banana.persistence", "com.banana.services"})
 //@Import({StudentsRepository.class, StudentsService.class})
-@PropertySource("classpath:application.properties")
 public class SpringConfig {
     /*@Bean
     public static PropertySourcesPlaceholderConfigurer propertyConfigurer() {
--- SchoolManager/src/test/java/com/banana/persistence/StudentsRepositoryTest.java
+++ SchoolManager/src/test/java/com/banana/persistence/StudentsRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -15,6 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 //@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {SpringConfig.class})
+@ActiveProfiles("dev")
 class StudentsRepositoryTest {
 
     @Autowired
