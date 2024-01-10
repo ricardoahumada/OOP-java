4db38603448273f47ac110694734eeeba654d4c8
ricardo <ricardo@enmotionvalue.com>
2024-01-10 13:34:09 +0100

    upddate

--- SchoolManagerSpringJPA/src/main/java/com/banana/persistence/StudentsRepository.java
+++ SchoolManagerSpringJPA/src/main/java/com/banana/persistence/StudentsRepository.java
@@ -3,11 +3,7 @@ package com.banana.persistence;
 import com.banana.models.Student;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 
-import javax.inject.Named;
 import java.util.ArrayList;
 import java.util.List;
 
