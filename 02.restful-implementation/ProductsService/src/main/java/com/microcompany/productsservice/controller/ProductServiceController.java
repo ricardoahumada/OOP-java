8f3b87206130de48b91abbbb919a8da64ad6385f
ricardo <ricardo@enmotionvalue.com>
2024-02-07 14:04:26 +0100

    update

--- 02.restful-implementation/ProductsService/src/main/java/com/microcompany/productsservice/controller/ProductServiceController.java
+++ 02.restful-implementation/ProductsService/src/main/java/com/microcompany/productsservice/controller/ProductServiceController.java
@@ -1,10 +1,32 @@
 package com.microcompany.productsservice.controller;
 
+import com.microcompany.productsservice.model.Product;
+import com.microcompany.productsservice.persistence.ProductsRepository;
+import com.microcompany.productsservice.service.ProductsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public class ProductServiceController {
     private static final Logger logger = LoggerFactory.getLogger(ProductServiceController.class);
 
+    @Autowired
+    private ProductsService service;
+
+    @Autowired
+    private ProductsRepository repo;
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public List<Product> getAll() {
+//        return service.getProductsByText("");
+        return repo.findAll();
+    }
 
 }
