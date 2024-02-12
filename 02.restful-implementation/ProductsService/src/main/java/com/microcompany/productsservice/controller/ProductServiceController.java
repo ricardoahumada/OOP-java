1d4dfc465934db94225459c934dbd5865cba88c2
ricardo <ricardo@enmotionvalue.com>
2024-02-12 13:40:00 +0100

    update

--- 02.restful-implementation/ProductsService/src/main/java/com/microcompany/productsservice/controller/ProductServiceController.java
+++ 02.restful-implementation/ProductsService/src/main/java/com/microcompany/productsservice/controller/ProductServiceController.java
@@ -12,6 +12,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @RestController
@@ -24,6 +26,9 @@ public class ProductServiceController {
 
     @Autowired
     private ProductsRepository repo;
+
+    @PersistenceContext
+    EntityManager em;
 
 //    @RequestMapping(value = "", method = RequestMethod.GET)
     /*@GetMapping("")
@@ -73,8 +78,10 @@ public class ProductServiceController {
     @PostMapping(value = "/duplicarProducto/{pid}")
     public ResponseEntity<Product> duplicate(@PathVariable Long pid) {
         Product currProd = repo.findById(pid).get();
-        Product newProduct = new Product(null, currProd.getName(), currProd.getSerial());
-        return new ResponseEntity<>(repo.save(newProduct), HttpStatus.CREATED);
+        em.detach(currProd);
+//        Product newProduct = new Product(null, currProd.getName(), currProd.getSerial());
+        currProd.setId(null);
+        return new ResponseEntity<>(repo.save(currProd), HttpStatus.CREATED);
     }
 
 }
