ad022c810054dbca47183cda30020a4ca8d96ae9
ricardo <ricardo@enmotionvalue.com>
2024-02-12 13:04:12 +0100

    update

--- 02.restful-implementation/ProductsService/src/main/java/com/microcompany/productsservice/controller/ProductServiceController.java
+++ 02.restful-implementation/ProductsService/src/main/java/com/microcompany/productsservice/controller/ProductServiceController.java
@@ -7,6 +7,8 @@ import com.microcompany.productsservice.service.ProductsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,15 +24,28 @@ public class ProductServiceController {
     @Autowired
     private ProductsRepository repo;
 
-    @RequestMapping(value = "", method = RequestMethod.GET)
+//    @RequestMapping(value = "", method = RequestMethod.GET)
+    /*@GetMapping("")
     public List<Product> getAll() {
         return repo.findAll();
+    }*/
+
+    @GetMapping("")
+    public ResponseEntity<List<Product>> getAll() {
+//        return repo.findAll();
+        return new ResponseEntity<>(repo.findAll(), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "", method = RequestMethod.POST)
+    /*@RequestMapping(value = "", method = RequestMethod.POST)
     public Product save(@RequestBody Product newProduct) {
         logger.info("newProduct:" + newProduct);
         return repo.save(newProduct);
+    }*/
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public ResponseEntity<Product> save(@RequestBody Product newProduct) {
+        logger.info("newProduct:" + newProduct);
+        return new ResponseEntity<>(repo.save(newProduct), HttpStatus.CREATED);
     }
 
     @RequestMapping(value = "/{pid}", method = RequestMethod.GET)
@@ -39,14 +54,17 @@ public class ProductServiceController {
     }
 
     @RequestMapping(value = "/{pid}", method = RequestMethod.DELETE)
-    public void delete(@PathVariable("pid") Long id) {
+    public ResponseEntity delete(@PathVariable("pid") Long id) {
         repo.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
-    @RequestMapping(value = "/{pid}", method = RequestMethod.PUT)
-    public Product update(@PathVariable("pid") Long id, @RequestBody Product product) {
-        if (id == product.getId()) return repo.save(product);
-        else throw new RuntimeException();
+    //    @RequestMapping(value = "/{pid}", method = RequestMethod.PUT)
+    @PutMapping("/{pid}")
+    public ResponseEntity<Product> update(@PathVariable("pid") Long id, @RequestBody Product product) {
+        if (id == product.getId()) {
+            return new ResponseEntity<>(repo.save(product), HttpStatus.ACCEPTED);
+        } else throw new RuntimeException();
     }
 
 }
