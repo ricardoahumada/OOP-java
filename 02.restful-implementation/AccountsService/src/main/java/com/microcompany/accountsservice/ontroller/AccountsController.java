d201b09de9edea0eebaf0b40e2bd7a2774cc9912
ricardonetmind <ricardoag@expert.netmind.es>
2024-02-19 09:12:55 +0100

    update

--- 02.restful-implementation/AccountsService/src/main/java/com/microcompany/accountsservice/ontroller/AccountsController.java
+++ 02.restful-implementation/AccountsService/src/main/java/com/microcompany/accountsservice/ontroller/AccountsController.java
@@ -44,4 +44,58 @@ public class AccountsController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
+
+    // update account
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Account> updateAccount(
+            @RequestBody @Valid Account account,
+            @PathVariable @Min(1) Long id
+    ) {
+        account.setId(id);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(accountService.updateAccount(id, account));
+    }
+
+    // Add Money
+    @PutMapping("/addmoney/{id}")
+    public ResponseEntity<Account> addMoney(
+            @PathVariable @Min(1) Long id,
+            @RequestParam @Min(1) int amount,
+            @RequestParam @Min(1) Long ownerId
+    ) {
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(accountService.addBalance(id, amount, ownerId));
+    }
+
+    // withdraw Money
+    @PutMapping("/withdraw/{id}")
+    public ResponseEntity<Account> withdraw(
+            @PathVariable @Min(1) Long id,
+            @RequestParam @Min(1) int amount,
+            @RequestParam @Min(1) Long ownerId
+    ) {
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(accountService.withdrawBalance(id, amount, ownerId));
+    }
+
+    // Delete Account
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public ApiResponse deleteAccount(
+            @PathVariable @Min(1) Long id
+    ) {
+        this.accountService.delete(id);
+        return new ApiResponse("Account is Successfully Deleted", true);
+    }
+
+    // Delete Account using ownerId
+
+    @DeleteMapping("user/{ownerId}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public ApiResponse deleteAccountByUserId(
+            @PathVariable @Min(1) Long ownerId
+    ) {
+        this.accountService.deleteAccountsUsingOwnerId(ownerId);
+        return new ApiResponse(" Accounts with given userId is deleted Successfully", true);
+
+    }
 }
