497140541b35ff4eb94c639920d41fa0f32610ea
ricardonetmind <ricardoag@expert.netmind.es>
2024-03-04 17:51:45 +0100

    update

--- Bananamint/src/main/java/com/banana/bananamint/services/GoalService.java
+++ Bananamint/src/main/java/com/banana/bananamint/services/GoalService.java
@@ -14,6 +14,7 @@ public interface GoalService {
     public List<Goal> add(Long idCustomer, Goal goal) throws GoalException;
 
     public List<GoalApproximation> generateReport(Long idCustomer, LocalDate initDate, LocalDate finalDate) throws GoalException;
+
     public List<Debt> accumulatedDebt(Long idCustomer, LocalDate initDate, LocalDate finalDate) throws GoalException;
 
 }
